761aa9419adde63d5be4d0365800d5a5d52c6c0c
Tyler <bulldogboy912000@yahoo.com>
2015-06-08 18:58:09 -0500

    Initial Commit

---
+++ lua/autorun/init.lua
@@ -0,0 +1,6 @@
+AddCSLuaFile( "cl_init.lua" ) --Tell the server that the client needs to download cl_init.lua
+AddCSLuaFile( "shared.lua" ) --Tell the server that the client needs to download shared.lua
+
+include( 'shared.lua' ) --Tell the server to load shared.lua
+
+
---
+++ lua/autorun/shared.lua
@@ -0,0 +1,62 @@
+--local CMoveData = FindMetaTable( "CMoveData" )
+local function calcSetupMove(ply, mv)
+	
+	if(CAMERA_ON == false) then
+		return
+	end
+
+	--if(mv:KeyDown(IN_MOVELEFT)) then
+	if mv:GetVelocity() != nil and mv:GetVelocity() != Vector(0, 0, 0)then
+		---mv:GetAngles().y
+		new_angle = Angle(0, 0, 0)
+		mv:SetMoveAngles(new_angle)
+	end
+	
+	// if(mv:KeyDown( IN_ATTACK )) then
+	// 	mv:AddKey( IN_ATTACK )
+	// 	print("Fire")
+	// 	ply:Fire()
+	// 	RunConsoleCommand( "+attack" )
+	// end
+
+	--print(mv:GetAngles())
+	--mv:SetAngles(Angle(0, 0, 0))
+	return mv
+end
+hook.Add("SetupMove", "zoba.calcSetupMove", calcSetupMove)
+
+
+
+
+
+
+if SERVER then
+	local function startPlayerCamera(ply)
+		umsg.Start("startcam", pl)
+		umsg.End()
+	end
+
+	local function stopPlayerCamera(ply)
+		umsg.Start("stopcam", ply)
+		umsg.End()
+	end
+
+	local function playerSpawn( ply )
+		startPlayerCamera(ply)
+	end
+	hook.Add("PlayerSpawn", "zoba.playerSpawn", playerSpawn)
+
+	local function playerDeath( ply )
+
+		stopPlayerCamera(ply)
+	end
+
+	hook.Add("PlayerDeath", "zoba.playerDeath", playerDeath)
+
+
+
+	concommand.Add( "zoba_start", startCam)
+	concommand.Add( "zoba_stop", stopCam)
+end
+
+
